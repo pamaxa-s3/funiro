37e6641bb3a38536ad2af2993ff25e471ca4d5e5
Roman Strynzha <pamaxas3@gmail.com>
2022-01-03 17:55:50 +0200

    add slider main

--- Funiro/js/app.js
+++ Funiro/js/app.js
@@ -196,7 +196,8 @@ if (sliders) {
 	}
 	sliders_bild_callback();
 }
-function sliders_bild_callback(params) { }
+
+function sliders_bild_callback(params) {}
 
 let sliderScrollItems = document.querySelectorAll('._swiper_scroll');
 if (sliderScrollItems.length > 0) {
@@ -223,95 +224,33 @@ if (sliderScrollItems.length > 0) {
 }
 
 
-function sliders_bild_callback(params) { }
+function sliders_bild_callback(params) {}
 
 if (document.querySelector('.slider-main__body')) {
 	new Swiper('.slider-main__body', {
 		observer: true,
 		observeParents: true,
 		slidesPerView: 1,
-		spaceBetween: 32,
+		spaceBetween: 32, //* Расстояние мехду слайдами по макету
 		watchOverflow: true,
 		speed: 800,
 		loop: true,
 		loopAdditionalSlides: 5,
 		preloadImages: false,
 		parallax: true,
-		// Dotts
+
+		//?Dots
 		pagination: {
-			el: '.controls-slider-main__dotts',
-			clickable: true,
+			el: '.controls-slider-main__dots',
+			clickable: true
 		},
-		// Arrows
+
+		//? Arrows
 		navigation: {
 			nextEl: '.slider-main .slider-arrow_next',
-			prevEl: '.slider-main .slider-arrow_prev',
+			prevEl: '.slider-main .slider-arrow_prev'
 		}
 	});
-}
-
-if (document.querySelector('.slider-rooms__body')) {
-	new Swiper('.slider-rooms__body', {
-		observer: true,
-		observeParents: true,
-		slidesPerView: 'auto',
-		spaceBetween: 24,
-		speed: 800,
-		loop: true,
-		watchOverflow: true,
-		loopAdditionalSlides: 5,
-		preloadImages: false,
-		parallax: true,
-		// Dotts
-		pagination: {
-			el: '.slider-rooms__dotts',
-			clickable: true,
-		},
-		// Arrows
-		navigation: {
-			nextEl: '.slider-rooms .slider-arrow_next',
-			prevEl: '.slider-rooms .slider-arrow_prev',
-		}
-	});
-}
-
-if (document.querySelector('.slider-tips__body')) {
-	new Swiper('.slider-tips__body', {
-		observer: true,
-		observeParents: true,
-		slidesPerView: 3,
-		spaceBetween: 32,
-		speed: 800,
-		loop: true,
-		watchOverflow: true,
-		// Dotts
-		pagination: {
-			el: '.slider-tips__dotts',
-			clickable: true,
-		},
-		// Arrows
-		navigation: {
-			nextEl: '.slider-tips .slider-arrow_next',
-			prevEl: '.slider-tips .slider-arrow_prev',
-		},
-		breakpoints: {
-			// when window width is >= 320px
-			320: {
-				slidesPerView: 1.1,
-				spaceBetween: 15
-			},
-			// when window width is >= 768px
-			768: {
-				slidesPerView: 2,
-				spaceBetween: 20
-			},
-			// when window width is >= 992px
-			992: {
-				slidesPerView: 3,
-				spaceBetween: 32
-			}
-		}
-	})
 }
 var ua = window.navigator.userAgent;
 var msie = ua.indexOf("MSIE ");
@@ -1123,6 +1062,22 @@ window.onload = function () {
          document.querySelector('.search-form').classList.remove('_active');
       }
    }
+
+   // * Header 
+
+   const headerElement = document.querySelector('.header');
+
+   const callback = function (entries, observer) {
+      if (entries[0].isIntersecting) {
+         headerElement.classList.remove('_scroll');
+      } else {
+         headerElement.classList.add('_scroll');
+      }
+   }
+
+   const headerObserver = new IntersectionObserver(callback);
+
+   headerObserver.observe(headerElement);
 }
 //let btn = document.querySelectorAll('button[type="submit"],input[type="submit"]');
 let forms = document.querySelectorAll('form');
